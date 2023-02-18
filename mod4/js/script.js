0edec6133c950df96ec8a2d708fbca3576681c8b
esixce <98708432+esixce@users.noreply.github.com>
2023-02-18 13:26:58 -0500

    final version mod4

--- mod4/js/script.js
+++ mod4/js/script.js
@@ -21,45 +21,17 @@ Hello Paula
 Hello Laura
 Good Bye Jim
 
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
-
 */
-
-// STEP 1:
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
 
 (function () {
 
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-  // STEP 10:
-  // Loop over the names array and say either 'Hello' or "Good Bye"
-  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
-  // See Lecture 50, part 1
-  /* fill in parts of the 'for' loop to loop over names array */
+  console.log('Part 1')
   for (var i = 0; i < names.length; i++) {
 
-    // STEP 11:
-    // Retrieve the first letter of the current name in the loop.
-    // Use the string object's 'charAt' function. Since we are looking for
-    // names that start with either upper case or lower case 'J'/'j', call
-    // string object's 'toLowerCase' method on the result so we can compare
-    // to lower case character 'j' afterwards.
-    // Look up these methods on Mozilla Developer Network web site if needed.
     var firstLetter = names[i].charAt(0).toLowerCase();
 
-    // STEP 12:
-    // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-    // name in the loop.
-    /* fill in condition here */
     if (firstLetter === 'j') {
       window.byeSpeaker.speak(names[i]);
     } else {
@@ -67,16 +39,54 @@ WARNING!!! WARNING!!!
     }
   }
 
-  var helloSpeaker = function (name) {
-    console.log('Welcome, ' + name);
-    return;
-  }
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+  console.log('\nPart 2 - map')
 
-  var byeSpeaker = function (name) {
-    console.log('Goodbye, ' + name);
-    return;
+  var findGreeting = function (name) {
+    var firstLetter = name.charAt(0).toLowerCase();
+    
+    if (firstLetter === 'j') {
+      return window.byeSpeaker.speakSimple(name);
+    } else {
+      return window.helloSpeaker.speakSimple(name);
+    }
+  };
+
+  var nameMap = names.map(name => findGreeting(name))
+  for (var i = 0; i < nameMap.length; i++) {
+    console.log(nameMap[i]);
+  } 
+
+  console.log('\nPart 3 - reduce')
+
+  var twoLists = names.reduce((total, name, index, array) => {
+
+    var firstLetter = name.charAt(0).toLowerCase();
+
+    var greeting = findGreeting(name);
+    
+    if (firstLetter === 'j') {
+      total.bye.push(greeting)
+    } else {
+      total.hello.push(greeting)
+    }
+
+    return total
+
+  }, {hello: [], bye: []});
+
+  for (var aList in twoLists) {
+    for(var name in twoLists[aList]) {
+      console.log(twoLists[aList][name]);
+    }
   }
 
 })();
+
+
+
+
+
+
 
 
